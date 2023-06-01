81edf8496b517c9b940cea517dcd878db99968df
Jessica Bealey <jfbealey@gmail.com>
2023-06-01 16:41:59 +0100

    Challenge-5 exercise complete

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -10,4 +10,31 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "Get /albums" do
+    it "lists all albums" do
+      response = get('/albums')
+      expected_response = 'Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq (expected_response)
+
+
+    end
+  
+  end
+
+  context "POST /album" do
+    it 'creates a new album' do
+
+      response = post('/album', title: 'Voyage', release_year: '2022', artist_id: '2')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      get_response = get('/albums')
+
+      expect(get_response.body).to include('Voyage')
+    end
+  end
+
 end
